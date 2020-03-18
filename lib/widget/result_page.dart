7deffdfa05b02f6b48edb0e23746dc3c9f104017
tamaoki <treasure6hunter9cole@gmail.com>
2020-03-18 19:17:08 +0900

    リセット画面のデザインの実装

--- lib/widget/result_page.dart
+++ lib/widget/result_page.dart
@@ -1,19 +1,53 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_quiz_app/utils/hex_color.dart';
 
 class ResultPage extends StatelessWidget {
-
+  /// タップ時の処理
   final Function _tapResetButton;
 
-  ResultPage(this._tapResetButton);
+  /// 全問題数
+  final int questionCount;
+
+  /// 正答数
+  final int correctAnswerCount;
+
+  ResultPage(
+      this._tapResetButton,
+      this.questionCount,
+      this.correctAnswerCount
+      );
 
   @override
   Widget build(BuildContext context) {
     return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        Text('問題が終了しました!'),
-        RaisedButton(
-          child: Text('リセットする'),
-          onPressed: _tapResetButton,
+        Padding(
+          padding: EdgeInsets.all(20),
+          child: Text('終了',
+              style: TextStyle(fontSize: 17, fontWeight: FontWeight.bold)),
+        ),
+        Padding(
+          padding: EdgeInsets.all(20),
+          child: Text(
+            '正答数: 1 / 2',
+            style: TextStyle(fontSize: 17, fontWeight: FontWeight.bold),
+          ),
+        ),
+        Padding(
+          padding: EdgeInsets.all(20),
+          child: Container(
+            height: 50,
+            width: MediaQuery.of(context).size.width - 100,
+            child: RaisedButton(
+              color: HexColor('#6DDE00'),
+              child: Text(
+                'リセットする',
+                style: TextStyle(color: Colors.white, fontSize: 17),
+              ),
+              onPressed: _tapResetButton,
+            ),
+          ),
         )
       ],
     );
